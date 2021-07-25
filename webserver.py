0d78bded720063a6211820606bc88a8c0e23f672
Alizain Fatehali <pl246901@ahschool.com>
2021-07-25 14:18:52 +0000

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,1 +1,20 @@
+from flask import Flask, escape, request, render_template, Response
+import time
+import socket
+import io
+import logging
+import os
+import threading
+from threading import Condition
 
+
+app=Flask(__name__)
+
+
+#HOME HTML TEMPLATE
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('Diversity.html')
+
+app.run(host= '0.0.0.0', port=8080, debug=True, threaded=True)
