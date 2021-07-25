0cdc4e2792719cab560bbc3704ebc0e5b2da9e65
Mudit Marwaha <pl238462@ahschool.com>
2021-07-25 04:37:06 +0000

    Update tabs.js

--- website/tabs.js
+++ website/tabs.js
@@ -14,3 +14,20 @@ function openPage(pageName,elmnt,color) {
 
 	// Get the element with id="defaultOpen" and click on it
 	document.getElementById("defaultOpen").click();
+	
+function openCity(evt, cityName) {
+		  var i, tabcontents, tablinkss;
+		  tabcontents = document.getElementsByClassName("tabcontents");
+		  for (i = 0; i < tabcontents.length; i++) {
+			tabcontents[i].style.display = "none";
+		  }
+		  tablinkss = document.getElementsByClassName("tablinkss");
+		  for (i = 0; i < tablinkss.length; i++) {
+			tablinkss[i].className = tablinkss[i].className.replace(" active", "");
+		  }
+		  document.getElementById(cityName).style.display = "block";
+		  evt.currentTarget.className += " active";
+		}
+
+		// Get the element with id="defaultOpen" and click on it
+		document.getElementById("defaultsOpen").click();
